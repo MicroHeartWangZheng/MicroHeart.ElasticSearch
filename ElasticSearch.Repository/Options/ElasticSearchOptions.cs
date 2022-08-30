08ea0b48162f8635ec8b81c9652ffa5df110a16b
王郑 <418706636@qq.com>
2022-08-30 11:22:34 +0800

    添加配置参数

--- ElasticSearch.Repository/Options/ElasticSearchOptions.cs
+++ ElasticSearch.Repository/Options/ElasticSearchOptions.cs
@@ -10,5 +10,10 @@ namespace ElasticSearch.Repository
         /// 索引前缀(防止索引名称重复)
         /// </summary>
         public string IndexPrefix { get; set; }
+
+        /// <summary>
+        /// 禁用直流式传输
+        /// </summary>
+        public bool DisableDirectStreaming { get; set; }
     }
 }
